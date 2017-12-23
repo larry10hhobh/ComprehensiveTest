77dc9601e2028e0a9ba67023586029a25a5b3f88
Larry <larry10hhobh@gmail.com>
2017-12-23 15:28:42 +0800

    优化碎片Fragment的方式切换(add、show、hide)。

--- app/src/main/java/com/example/comprehensivetest/MainActivity.java
+++ app/src/main/java/com/example/comprehensivetest/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.comprehensivetest;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.example.comprehensivetest.Frag.Fragment1;
@@ -15,10 +17,11 @@ import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
 import com.luseen.luseenbottomnavigation.BottomNavigation.OnBottomNavigationItemClickListener;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnBottomNavigationItemClickListener {
 
     private Fragment1 fragment1;
     private Fragment2 fragment2;
+    private FragmentTransaction mFragmentTransaction;
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -33,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
             bottomNavigationView.disableShadow();
             bottomNavigationView.setItemActiveColorWithoutColoredBackground(getResources().getColor(R.color.secondColor));
         }
-
         BottomNavigationItem bottomNavigationItem = new BottomNavigationItem
                 ("水果", getResources().getColor(R.color.secondColor), R.drawable.ic_mic_black_24dp);
         BottomNavigationItem bottomNavigationItem1 = new BottomNavigationItem
@@ -47,42 +49,61 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.addTab(bottomNavigationItem1);
         bottomNavigationView.addTab(bottomNavigationItem2);
         bottomNavigationView.addTab(bottomNavigationItem3);
-
-
-        bottomNavigationView.setOnBottomNavigationItemClickListener(new OnBottomNavigationItemClickListener() {
-            @Override
-            public void onNavigationItemClick(int index) {
-                Toast.makeText(MainActivity.this, "Item " + index + " clicked", Toast.LENGTH_SHORT).show();
-                switch (index) {
-                    case 0:
-                        chickFragment1();
-                        break;
-                    case 1:
-                        chickFragment2();
-                        break;
-                    case 2:
-//                                chickFragment1();
-                        break;
-                    case 3:
-//                                chickFragment1();
-                        break;
-                }
-            }
-        });
+        bottomNavigationView.setOnBottomNavigationItemClickListener(this);
 
     }
 
-    private void chickFragment1() {
-        fragment1 = new Fragment1();
-        FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.frag_content, fragment1);
+    @Override
+    public void onNavigationItemClick(int index) {
+//        Toast.makeText(MainActivity.this, "Item " + index + " clicked", Toast.LENGTH_SHORT).show();
+        //每次点击事件都会重新获取FragmentTransaction
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (index) {
+            case 0:
+                chickFragment1(mFragmentTransaction);
+                break;
+            case 1:
+                chickFragment2(mFragmentTransaction);
+                break;
+        }
+    }
+
+
+    private void chickFragment1(FragmentTransaction fragmentTransaction) {
+        if (fragment1 == null) {
+            fragment1 = new Fragment1();
+            fragmentTransaction.add(R.id.frag_content, fragment1);
+//            fragmentTransaction.replace(R.id.frag_content, fragment1);
+        }
+        hideAllFragment(fragmentTransaction);
+        fragmentTransaction.show(fragment1);
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         fragmentTransaction.commit();
     }
 
-    private void chickFragment2() {
-        fragment2 = new Fragment2();
-        FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.frag_content, fragment2);
+    private void chickFragment2(FragmentTransaction fragmentTransaction) {
+        if (fragment1 == null) {
+            fragment2 = new Fragment2();
+            fragmentTransaction.add(R.id.frag_content, fragment2);
+//            fragmentTransaction.replace(R.id.frag_content,fragment2);
+        }
+        hideAllFragment(fragmentTransaction);
+        fragmentTransaction.show(fragment2);
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         fragmentTransaction.commit();
+    }
+
+    /**
+     * 隐藏所有fragment
+     *
+     * @param fragmentTransaction
+     */
+    private void hideAllFragment(FragmentTransaction fragmentTransaction) {
+        if (fragment1 != null) {
+            fragmentTransaction.hide(fragment1);
+        }
+        if (fragment2 != null) {
+            fragmentTransaction.hide(fragment2);
+        }
     }
 }
